83a3084b2855be50ee34e02f57ac4eed6da7ddc4
sasame <sasametakaaki@gmail.com>
2019-12-05 14:30:45 +0900

    PHP03提出課題

---
+++ PHP03.php
@@ -0,0 +1,45 @@
+<?php
+
+//課題1
+  $name = "Sasame";
+
+  if ($name == "Sasame") {
+    echo "私は $name です";
+  } else {
+    echo "あなたの名前ではありません";
+  }
+  echo "\n";
+
+//課題2
+  $total = 0;
+
+  for ($i=0; $i <= 10000; $i++) {
+    $total += $i;
+  }
+  echo $total;
+  echo "\n";
+
+//課題3
+  $fruits = array("apple","peach","grape","orange","banana");
+
+  foreach ($fruits as $a) {
+    echo "要素は" . $a;
+    echo "\n";
+  }
+
+//課題4
+/* for文の始めの値を定義する */
+  $start = 1;
+/* for文の終わりの値を定義する*/
+  $end = 100;
+
+for ($i = $start; $i <= $end; $i++) {
+
+  //5で割り切れたら{}内を実行する
+  if ($i % 5 == 0) {
+    echo $i;
+    echo "\n";
+  }
+}
+
+?>
