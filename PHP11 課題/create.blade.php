cd59d960c56e28692be86550700d2a9927dab507
sasame <sasametakaaki@gmail.com>
2019-12-07 15:42:20 +0900

    PHP11の課題提出

---
+++ PHP11 課題/create.blade.php
@@ -0,0 +1,16 @@
+<!-- PHP11 課題4 -->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>プロフィール作成</title>
+  </head>
+  <body>
+    <h1>プロフィール作成画面</h1>
+    <p>このファイルはresources/views/admin/profileの中に作成しました</p>
+  </body>
+</html>
---
+++ PHP11 課題/edit.blade.php
@@ -0,0 +1,16 @@
+<!-- PHP11 課題4 -->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>プロフィール編集</title>
+  </head>
+  <body>
+    <h1>プロフィール編集画面</h1>
+    <p>このファイルはresources/views/admin/profileの中に作成しました</p>
+  </body>
+</html>
