c04d221639f16c5c51a76a02ac841198649e3e7c
sasame <sasametakaaki@gmail.com>
2019-12-10 15:31:03 +0900

    PHP14課題提出

---
+++ PHP14/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+// PHP14 課題3の追加
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+// PHP14 課題6の追加
+    Route::post('profile/edit', 'Admin\ProfileController@update');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
